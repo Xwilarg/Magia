8782336529cef364664e655803745acc8546bbd8
Xwilarg <xwilarg@protonmail.com>
2025-02-11 14:25:57 +0000

    Fix MCF export

--- src/Export/McfExporter.cpp
+++ src/Export/McfExporter.cpp
@@ -8,7 +8,7 @@ namespace Magia
 {
 	void McfExporter::Export(const std::string& path, int width, int height, std::vector<std::shared_ptr<DrawLayer>>& layers)
 	{
-		std::ofstream fs(path, std::ios::out | std::ios::binary | std::ios::app);
+		std::ofstream fs(path, std::ios::out | std::ios::binary | std::ios::trunc);
 
 		fs.write(reinterpret_cast<const char*>(&width), sizeof(width));
 		fs.write(reinterpret_cast<const char*>(&height), sizeof(height));
@@ -16,7 +16,7 @@ namespace Magia
 		fs.write(reinterpret_cast<const char*>(&layerCount), sizeof layerCount);
 		for (auto& l : layers)
 		{
-			char pxCount = 0;
+			unsigned char pxCount = 0;
 			uint32_t lastPixel;
 			for (int i = 0; i < width * height; i++)
 			{
@@ -24,9 +24,9 @@ namespace Magia
 				if (i == 0)
 				{
 					lastPixel = value;
-					pxCount++;
+					pxCount = 1;
 				}
-				else if (value == lastPixel)
+				else if (value == lastPixel && pxCount < UCHAR_MAX)
 				{
 					pxCount++;
 				}
@@ -50,8 +50,46 @@ namespace Magia
 
 		int width, height;
 		fs.read(reinterpret_cast<char*>(&width), sizeof(width));
-		fs.read(reinterpret_cast<char*>(&width), sizeof(width));
+		fs.read(reinterpret_cast<char*>(&height), sizeof(height));
+		unsigned short layerCount;
+		fs.read(reinterpret_cast<char*>(&layerCount), sizeof(layerCount));
 
 		layers.clear();
+
+		int max = width * height;
+
+		for (int l = 0; l < layerCount; l++)
+		{
+			std::shared_ptr<DrawLayer> layer = std::make_shared<DrawLayer>();
+
+			int i = 0;
+			while (i < max)
+			{
+				unsigned char adv;
+				uint32_t pixel;
+				fs.read(reinterpret_cast<char*>(&adv), sizeof(adv));
+				fs.read(reinterpret_cast<char*>(&pixel), sizeof(pixel));
+
+				if (adv == 0)
+				{
+					std::runtime_error("Invalid size 0");
+				}
+
+				for (int px = 0; px < adv; px++)
+				{
+					if (i + px >= width * height)
+					{
+						std::runtime_error("Out of bounds");
+					}
+					layer->Set(i + px, pixel);
+				}
+
+				i += adv;
+			}
+
+			layers.push_back(std::move(layer));
+		}
+
+		fs.close();
 	}
 }
